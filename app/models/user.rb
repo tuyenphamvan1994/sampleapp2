cf9b87e2d92872b92d3fc4a9bb3029df4f57ca8b
Pham Tuyen <tuyenphamvan1994@gmail.com>
2017-01-15 18:00:36 +0700

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :microposts, dependent: :destroy
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save {email.downcase!}
   before_create :create_activation_digest
@@ -65,4 +66,9 @@ class User < ApplicationRecord
     self.activation_token = User.new_token
     self.activation_digest = User.digest activation_token
   end
+
+  def feeds
+    self.microposts.order_by_time
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,17 @@
 User.create! name: "Pham Tuyen", email: "tuyenphamvan1994@gmail.com", password: "pt0987654321",
   password_confirmation: "pt0987654321", admin: true, activated: true, activated_at: Time.zone.now 
 
-99.times do |n|
+9.times do |n|
   name = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
   User.create! name: name, email: email, password: password, password_confirmation: password,
     activated: true, activated_at: Time.zone.now
+
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(5)
+  users.each {|user| user.microposts.create!(content: content)}
+end
+
 end
